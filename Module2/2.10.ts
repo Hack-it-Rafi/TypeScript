700323e2b0d14db15c948a0feca1c86a4a850ee6
Hack-it-Rafi <zozorafi2016@gmail.com>
2024-05-07 14:37:02 +0600

    Mapped Types

---
+++ Module2/2.10.ts
@@ -0,0 +1,27 @@
+{
+    const arrOfNumbers:number[] = [1,2,3,4];
+
+    const arrOfStrings:string[] = arrOfNumbers.map((number)=>number.toString());
+
+    // console.log(arrOfStrings);
+
+    type AreaNumber = {
+        height: number;
+        width: Number
+    }
+
+    // type AreaString = {
+    //     [key in "height"|"width"]:string;
+    // }
+    type AreaString<T> = {
+        [key in keyof T]:T[key];
+    }
+
+    const area1:AreaString<{height:string;width:number;depth:number}>={
+        height:"100",
+        width:50,
+        depth:100
+    }
+
+    console.log(area1);
+}
