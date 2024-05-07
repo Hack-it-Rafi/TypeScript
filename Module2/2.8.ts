3d0d3d12e6bd39a324d680d4ce21947ab0290771
Hack-it-Rafi <zozorafi2016@gmail.com>
2024-05-07 12:46:08 +0600

    Asyncrhonous promise

--- Module2/2.8.ts
+++ Module2/2.8.ts
@@ -1,7 +1,24 @@
 {
-    const createPromise = ()=>{
-        return new Promise((resolve, reject)=>{
-            const data:string = "Something";
+    interface ToDo{
+        userId: number;
+        id: number;
+        title: string;
+        completed: boolean;
+    }
+    const getTodo = async():Promise<ToDo>=>{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+        const data:ToDo = await response.json();
+        return data;
+        // console.log(data);
+    }
+
+    getTodo();
+
+    interface Something  {something:string};
+    const createPromise = ():Promise<Something>=>{
+        return new Promise<{something: string}>((resolve, reject)=>{
+            const data:Something = {something:"Hello"};
 
             if(data){
                 resolve(data);
@@ -11,10 +28,11 @@
         });
     };
 
-    const ShowData = async()=>{
-        const data = await createPromise();
-        console.log(data);
+    const ShowData = async():Promise<Something>=>{
+        const data:Something = await createPromise();
+        // console.log(data);
+        return data;
     }
 
-    ShowData();
+    // console.log(ShowData());
 }
