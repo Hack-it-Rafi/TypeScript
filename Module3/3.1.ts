cead6ab8ec03124c24f3d5e04dfa35ebecb66519
Hack-it-Rafi <zozorafi2016@gmail.com>
2024-05-07 17:20:34 +0600

    Typescript class and object

---
+++ Module3/3.1.ts
@@ -0,0 +1,21 @@
+{
+    class Animal{
+        // public name: string;
+        // public species: string;
+        // public sound : string;
+
+        constructor(public name: string , public species: string, public sound:string){
+            // this.name = name;
+            // this.species = species;
+            // this.sound = sound;
+        }
+
+        makeSound(){
+            console.log(`The ${this.name} says ${this.sound}`);
+        }
+    }
+
+    const dog = new Animal("Sorailla", "Kutta", "Grrrrrrr");
+
+    dog.makeSound();
+}
